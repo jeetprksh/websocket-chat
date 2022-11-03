9b90cf6f9c75ee27cc4ae4034ff65d9c2cb273fd
Jeet Prakash <jeet1989a@gmail.com>
2022-11-03 23:50:14 +0530

    Fixed compilation issue on client

--- client/src/app/service/app.service.ts
+++ client/src/app/service/app.service.ts
@@ -1,6 +1,6 @@
 import {Injectable}   from '@angular/core';
 import {XHRHandler}   from './xhrhandler.service';
-import {LoginRequest} from '../data/loginRequest'
+import {LoginRequest} from '../data/loginrequest'
 import {Observable}   from 'rxjs';
 
 @Injectable() 
