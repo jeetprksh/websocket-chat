bbb6b5a2e1b582e20f97ff3b61ed9ae4278eb739
Jeet Prakash <11135910+jeetprksh@users.noreply.github.com>
2025-12-31 17:24:35 +0530

    UI: Initial commit for new UI app

--- chat-ui/src/main.ts
+++ chat-ui/src/main.ts
@@ -1,7 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/app-module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
+platformBrowser().bootstrapModule(AppModule, {
+  
 })
   .catch(err => console.error(err));
